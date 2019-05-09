d983e5bb23cb15e1d4579ec15d297e672bf42db1
Tom Filepp <tom@paperscissorsandglue.com>
2019-05-08 17:30:21 -0700

    add basic workaround for returning results, so we have context for failed validations

--- src/Contracts/FeedValidatorInterface.php
+++ src/Contracts/FeedValidatorInterface.php
@@ -8,7 +8,9 @@ interface FeedValidatorInterface
      * Validate an atom or rss feed.
      *
      * @param string $feed
+     * @param bool $with_results
      * @return bool
      */
-    public function validate($feed);
+    public function validate($feed, $with_results);
+
 }
--- src/W3CFeedValidator.php
+++ src/W3CFeedValidator.php
@@ -6,13 +6,16 @@ use Fungku\MarkupValidator\Contracts\FeedValidatorInterface;
 
 class W3CFeedValidator implements FeedValidatorInterface
 {
+
+    public $is_valid = false;
+    public $results = [];
     /**
      * Validate the atom or rss feed.
      *
      * @param string $feed The url of the feed to validate.
      * @return bool
      */
-    public function validate($feed)
+    public function validate($feed, $with_results=false)
     {
         $validator = "http://validator.w3.org/feed/check.cgi?output=soap12&url={$feed}";
         $response = file_get_contents($validator);
@@ -21,7 +24,13 @@ class W3CFeedValidator implements FeedValidatorInterface
         $xml->loadXML($response);
 
         $validity = $xml->getElementsByTagName('validity');
+        $this->is_valid = $validity->length && $validity->item(0)->nodeValue == 'true';
+        $this->results = $xml;
+        // return the object, then we can just check both the validity and results if not valid
+        if ($with_results) {
+          return $this;
+        }
 
-        return $validity->length && $validity->item(0)->nodeValue == 'true';
+        return $this->is_valid;
     }
 }
